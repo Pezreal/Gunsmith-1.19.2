7c0a2d0df21da090e53ea23b11370df4a3b98968
Dean Jackson <deano303030@gmail.com>
2022-12-28 22:38:23 -0600

    Initial commit

---
+++ src/main/java/net/pezreal/gunsmith/Gunsmith.java
@@ -0,0 +1,15 @@
+package net.pezreal.gunsmith;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Gunsmith implements ModInitializer {
+	public static final String MOD_ID = "gunsmith";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/pezreal/gunsmith/GunsmithClient.java
@@ -0,0 +1,10 @@
+package net.pezreal.gunsmith;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GunsmithClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
